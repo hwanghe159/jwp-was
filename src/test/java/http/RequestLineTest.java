7d855d8ff99de85452f521a1ecb1919bf41da671
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-09-13 17:07:31 +0900

    test: 테스트 추가
    - RequestLine 생성 테스트 추가
    - Method 테스트 추가

---
+++ src/test/java/http/RequestLineTest.java
@@ -0,0 +1,18 @@
+package http;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLineTest {
+    @DisplayName("request line 문자열이 주어진 경우 적절하게 정보를 분배해야 한다")
+    @Test
+    void generateTest() {
+        RequestLine requestLine = new RequestLine("GET /index.html HTTP/1.1");
+
+        assertThat(requestLine.getMethod()).isEqualTo(Method.GET);
+        assertThat(requestLine.getRequestUri()).isEqualTo("/index.html");
+        assertThat(requestLine.getHttpVersion()).isEqualTo("HTTP/1.1");
+    }
+}
