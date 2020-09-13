8e912b9add5b9f870f333a4cbc8695920e7e35d5
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-09-13 16:34:01 +0900

    refactor: 표준 명칭에 맞게 클래스명 변경

--- src/main/java/http/Request.java
+++ src/main/java/http/Request.java
@@ -5,12 +5,12 @@ import java.io.BufferedReader;
 public class Request {
     private RequestLine requestLine;
     private RequestHeader requestHeader;
-    private RequestBody requestBody;
+    private MessageBody messageBody;
 
     public Request(BufferedReader br) throws Exception {
         this.requestLine = new RequestLine(br);
         this.requestHeader = new RequestHeader(br);
-        this.requestBody = new RequestBody(br, requestHeader.getContentLength());
+        this.messageBody = new MessageBody(br, requestHeader.getContentLength());
     }
 
     public RequestLine getRequestLine() {
@@ -21,11 +21,11 @@ public class Request {
         return requestHeader;
     }
 
-    public RequestBody getRequestBody() {
-        return requestBody;
+    public MessageBody getMessageBody() {
+        return messageBody;
     }
 
-    public boolean isMethod(RequestMethod requestMethod) {
-        return requestMethod == requestLine.getMethod();
+    public boolean isMethod(Method method) {
+        return method == requestLine.getMethod();
     }
 }
